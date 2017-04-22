f3d38f82d0ad34c2bb96f8bc2c506c7edf45c803
KoSeongjin <catch1302@naver.com>
2017-04-22 18:26:37 +0900

    4월 22일 오후 6시 26분 등록완료 고성진

--- [Project]BangGood/src/main/java/global/sesoc/banggood/dao/PropertyDAO.java
+++ [Project]BangGood/src/main/java/global/sesoc/banggood/dao/PropertyDAO.java
@@ -3,6 +3,7 @@ package global.sesoc.banggood.dao;
 import java.util.ArrayList;
 import java.util.Map;
 
+import global.sesoc.banggood.vo.Canvas;
 import global.sesoc.banggood.vo.Maintence;
 import global.sesoc.banggood.vo.Option;
 import global.sesoc.banggood.vo.Picture;
@@ -19,20 +20,25 @@ public interface PropertyDAO {
 
 	public int select_recently_property() throws Exception;  // 옵션과 관리비 내역 등록위해 최근 값 뽑아오기
 	
-	//삭제
-	// public int delete_property(int property_no) throws Exception; // 매물 삭제
+	//게시 중단
+	public int stop_showing(int property_no) throws Exception;
+	
+	//재게시
+	public int restart_showing(int property_no) throws Exception;
 
 	//매물 정보 읽어오기(상세읽기)
-	//public Property select_property_property(int property_no) throws Exception;  // 매물 상세내용 가져오기
-	//public Option select_property_option(int property_no) throws Exception;
-	//public Maintence select_property_maintence(int property_no) throws Exception;
+	public Property select_property_property(int property_no) throws Exception; 
+	public Option select_property_option(int property_no) throws Exception;
+	public Maintence select_property_maintence(int property_no) throws Exception;
+	
+	public ArrayList<Picture> select_property_picture(int property_no) throws Exception;
 	
 	//매물정보 수정
-	//public int update_property(Property property) throws Exception;  // 매물 수정
-	//public int update_option(Option option) throws Exception;  // 옵션 수정
-	//public int update_maintence(Maintence maintence) throws Exception;  // 관리비 수정
-	//public int delete_picture(Picture picture) throws Exception;  // 사진 삭제 후 재등록
-	//public int delete_floorplan(Floorplan floorplan) throws Exception;  // 평면도 삭제 후 재등록
+	public int update_property(Property property) throws Exception;  // 매물 수정
+	public int update_option(Option option) throws Exception;  // 옵션 수정
+	public int update_maintence(Maintence maintence) throws Exception;  // 관리비 수정
+	
+	public int update_picture(Picture picture) throws Exception;  // 사진 삭제 후 재등록
 
 	//조건조회
 	public ArrayList<String> searchProperty(Property_search search);
